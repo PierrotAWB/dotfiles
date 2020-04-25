c548442a76073ffe3b647791138c1aa791f023b4
Andrew Wang <andrewwang298@gmail.com>
2020-04-25 11:02:58 -0400

    Fix polybar and update config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+# Script to launch polybar bars
+
+
 # Terminate already running bar instances
 killall -q polybar
+
+# Source xresources on startup
+xrdb $HOME/.config/Xresources
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar(s)
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload bar-primary &
-	done
-else 
-	polybar --reload bar-primary &
-fi
+# Launch polybar on every monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  echo $m
+  if [ ! -z "$(polybar --list-monitors | awk -v m="$m" '$0 ~ m && /primary/')" ]; then
+    MONITOR=$m polybar --reload bar-primary &
+    echo primary
+  else
+    MONITOR=$m polybar --reload bar-secondary &
+    echo secondary
+  fi
+done
 
 echo "Bars launched..."
